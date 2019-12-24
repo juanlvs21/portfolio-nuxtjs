1f46ef2d9626091a7eef7110a681b1f9f24f5bdb
Juan Villarroel <=>
2019-12-23 21:37:24 -0400

    It was changed bg.png to bg.webp

---
+++ components/core/BgImg.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="tool-img" :style="{ height: height }"></div>
+</template>
+
+<script>
+export default {
+  name: 'BgImg',
+  props: ['height']
+}
+</script>
+
+<style>
+.tool-img {
+  width: 100%;
+  background-image: url('~assets/img/bg.webp');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+</style>
